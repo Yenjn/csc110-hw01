c1ea721642dbbf942d0fa15617fca31c36e962b6
Jenny Liang <y2007345@qq.com>
2026-09-24 15:01:45 -0400

    first try committing

--- hw01.py
+++ hw01.py
@@ -1,12 +1,12 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: (put your name here)JennyLiang
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
 
 
 def main():
-    """
+    """ #not going to be executed, only reference
     This is a Docstring for the main function. This is the short description.
 
     Here, after a blank line, you can add a longer paragraph description.
@@ -45,13 +45,35 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+    x=27
+    y=1
+    a=1.5
+    b=7
+    c=-1
+    result1 = (3*x-9*y)/(2*a*(b-c))
+    print("part 1: x=", x)
+    print("part 1: y=", y)
+    print("part 1: a=", a)
+    print("part 1: b=", b)
+    print("part 1: c=", c)
+    print("part 1: result=", result1)
 
     # End of Part 1 ----------------------
-
+    
 
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
+    x=5
+    y=-3
+    result2=x**2*y**4
+    print("part 2: x=", x)
+    print("part 2: y=", y)
+    print("part 2: result=", result2)
+    
+
+    
+
 
     # End of Part 2 ----------------------
 
@@ -60,6 +82,12 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
+    a=100
+    b=13
+    result3=a//b
+    print("part 3: a=", a)
+    print("part 3: b=", b)
+    print("part 3: result=", result3)
 
     # End of Part 3 ----------------------
 
@@ -67,6 +95,10 @@ def main():
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
+    a=100
+    b=13
+    result4=a%b
+    print("part 4: result=", result4)
 
     # End of Part 4 ----------------------
 
